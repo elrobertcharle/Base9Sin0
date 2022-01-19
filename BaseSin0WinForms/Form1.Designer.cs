0d9f3e55def3f3a3f9d2adabf4da180d7d7c9bd7
Roberto C. Castaño <elrobertcharle@outlook.com>
2022-01-19 18:25:06 -0500

    ok

--- BaseSin0WinForms/Form1.Designer.cs
+++ BaseSin0WinForms/Form1.Designer.cs
@@ -29,12 +29,102 @@ namespace BaseSin0WinForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Base9Sin0TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Base9TextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(236, 38);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(75, 23);
+            this.ConvertButton.TabIndex = 1;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Base9Sin0TextBox
+            // 
+            this.Base9Sin0TextBox.Location = new System.Drawing.Point(80, 96);
+            this.Base9Sin0TextBox.Name = "Base9Sin0TextBox";
+            this.Base9Sin0TextBox.ReadOnly = true;
+            this.Base9Sin0TextBox.Size = new System.Drawing.Size(150, 23);
+            this.Base9Sin0TextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Base10:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Base9Sin0:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Base9:";
+            // 
+            // Base9TextBox
+            // 
+            this.Base9TextBox.Location = new System.Drawing.Point(80, 67);
+            this.Base9TextBox.Name = "Base9TextBox";
+            this.Base9TextBox.ReadOnly = true;
+            this.Base9TextBox.Size = new System.Drawing.Size(150, 23);
+            this.Base9TextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(338, 202);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Base9TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Base9Sin0TextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ConvertButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Base9Sin0TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Base9TextBox;
     }
 }
--- BaseSin0WinForms/Form1.cs
+++ BaseSin0WinForms/Form1.cs
@@ -17,5 +17,57 @@ namespace BaseSin0WinForms
             InitializeComponent();
         }
 
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            int b9 = Base10ToBase9(int.Parse(textBox1.Text));
+            int b9sin0 = Base9ToBase9Sin0(b9);
+            Base9TextBox.Text = b9.ToString();
+            Base9Sin0TextBox.Text = b9sin0.ToString();
+        }
+
+        private int Base10ToBase9(int b10)
+        {
+            var b = 9;
+            var resultStr = "";
+            int lastExponent = 0;
+            while (true)
+            {
+                if (b10 < b)
+                {
+                    resultStr += b10.ToString().PadLeft(lastExponent, '0');
+                    break;
+                }
+                var exponent = (int)Math.Log(b10, b);
+                int d = b10 / (int)(Math.Pow(b, exponent));
+                if (lastExponent == 0)
+                    resultStr += d;
+                else
+                    resultStr += d.ToString().PadLeft(lastExponent - exponent, '0');
+                lastExponent = exponent;
+                b10 = b10 - d * (int)Math.Pow(b, exponent);
+            }
+
+            return int.Parse(resultStr);
+        }
+
+        public int Base9ToBase9Sin0(int b9)
+        {
+            var l = b9.ToString().AsEnumerable().Select(c => c - '0').ToArray();
+            bool carry = false;
+            for (var i = l.Length - 1; i >= 0; i--)
+            {
+                bool nextCarry = false;
+                if (l[i] == 0)
+                {
+                    l[i] = 9;
+                    nextCarry = true;
+                }
+                if (carry)
+                    l[i]--;
+                carry = nextCarry;
+            }
+            return int.Parse(string.Concat(l));
+        }
+
     }
 }
